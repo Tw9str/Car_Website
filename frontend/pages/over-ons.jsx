d802a2cc8a5926def8d0d70510d9d2ac0b4625ca
Tw9str <i88b@hotmail.com>
2023-03-13 15:44:55 +0100

    added contact and about pages

--- frontend/pages/over-ons.jsx
+++ frontend/pages/over-ons.jsx
@@ -6,17 +6,15 @@ export default function About() {
   return (
     <div className="about">
       <div className="container">
-          Welkom bij BCP&lsquo; het auto bedrijf waar u terecht kunt voor al uw autozaken.
+        <p>
+        We  lkom bij BCP&lsquo; het auto bedrijf waar u terecht kunt voor al uw autozaken.
           Bij BCP hebben we een passie voor auto&apos;s en staan we altijd klaar om u te helpen bij de aankoop&lsquo; verkoop&lsquo; inruil&lsquo; onderhoud en reparatie van uw auto.
           Of u nu op zoek bent naar een betrouwbare tweedehands auto&lsquo; uw huidige auto wilt inruilen voor een ander model&lsquo; of uw auto wilt laten onderhouden door ervaren monteurs&lsquo; bij BCP bent u aan het juiste adres.
           Wij bieden een persoonlijke aanpak&lsquo; een uitgebreide service en eerlijke prijzen.
           Ontdek ons ruime aanbod aan tweedehands auto&apos;s en onze professionele diensten op onze website en neem contact met ons op voor meer informatie.
-        <div className="social">
-          <Social/>
-        </div>
-        <div className="map">
-          <Map />
-        </div>
+        </p>
+        <Social/>
+        <Map />
       </div>
     </div>
   )
